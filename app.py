a25d3e665cb26710fe3b5a95fac2d105b1b51546
Jung Jaeyoung <jyihaan4@jbnu.ac.kr>
2023-10-28 07:01:14 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,19 +6,20 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 
-
 def main():
     data_dir = './output/weather'
 
     station_info = pd.read_csv('./input/종관기상_관측지점.csv')
     location_dict = dict(zip(station_info['지점명'], station_info['지점코드']))
 
-    select_list = list(location_dict.keys())
+    location_list = list(location_dict.keys())
 
-    selected = st.selectbox('data 선택', select_list)
-    print(location_dict[selected])
+    selected_location = st.selectbox('지역 선택', location_list)
 
-    df = pd.read_csv(os.path.join(data_dir, f'{location_dict[selected]}_2023.csv'))
+    year_list = [2023,]
+    selected_year = st.selectbox('연도 선택', year_list)
+
+    df = pd.read_csv(os.path.join(data_dir, f'{location_dict[selected_location]}_{selected_year}.csv'))
     df['year'] = pd.to_datetime(df['tm']).dt.year
     df['month'] = pd.to_datetime(df['tm']).dt.month
     df['day'] = pd.to_datetime(df['tm']).dt.day
@@ -30,6 +31,7 @@ def main():
     st.pyplot(fig)
 
     df = df.drop(["day", "month", "year"], axis=1)
+
     if st.button("Download CSV"):
         csv = df.to_csv(index=False)
         b64 = base64.b64encode(csv.encode()).decode()
